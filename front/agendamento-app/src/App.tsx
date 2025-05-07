315581aa8a06a3d3e414949a5c2370dd5f5447e4
Gustavo Ferreira Vargens <gustavofv3050@gmail.com>
2025-05-07 10:45:10 -0300

    Telas de Login

--- front/agendamento-app/src/App.tsx
+++ front/agendamento-app/src/App.tsx
@@ -1,24 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import SocialScreen from './templates/SocialScreen'
+import LoginScreen from './templates/LoginScreen'
+import SignUpFormUser from './templates/SignUpFormUser'
+import SignUpFormProfessional from './templates/SignUpFormProfessional'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+    {
+    // <SocialScreen />
+    // <LoginScreen />
+    // <SignUpFormUser />
+    <SignUpFormProfessional />
+    }
+
+  </div>
   )
 }
 
---
+++ front/agendamento-app/src/templates/SocialScreen.tsx
@@ -0,0 +1,35 @@
+import {FcGoogle} from 'react-icons/fc'
+import {FaFacebook} from 'react-icons/fa'
+import './SocialScreen.css'
+
+const LoginScreen = () => 
+    {
+        return (
+            <div className='login-container'>
+                <div className='login-box'>
+                    <h1 className='title'>HBGB</h1>
+                    <h1 className='text-content'>Book an Appointment for
+                    Salon, Spa & Barber
+                    </h1>
+                   
+
+                    <div className="social-login">
+                        <button className="google-btn">
+                            <FcGoogle className = 'icon' style={{marginRight: "25px"}}/>
+                            Continue com Google
+                        </button>
+
+                        <button className="facebook-btn">
+                            <FaFacebook className='icon'/>
+                            Continue com Facebook
+                        </button>
+                    </div>
+                    <div className="login-footer"> 
+                        <p>Já possui conta? <a href="/login">Entrar</a></p>
+                    </div>
+                </div>
+            </div>
+        )
+    } 
+
+    export default LoginScreen
