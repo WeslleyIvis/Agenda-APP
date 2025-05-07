1ffcdece102a9ff1767c3ea209e2b59cf110eac8
WeslleyIvis <weslley_reive@hotmail.com>
2025-05-07 10:44:02 -0300

    Config scss

--- front/agendamento-app/vite.config.ts
+++ front/agendamento-app/vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-        @import "@/assets/scss/main.scss"; 
-        @import "@/assets/scss/global.scss";
-        `
-      }
-    }
-  }
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
