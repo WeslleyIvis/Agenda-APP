0707c3dcd2b26bc2b5972acf5a461c5917b754b7
WeslleyIvis <weslley_reive@hotmail.com>
2025-05-17 03:28:56 -0300

    O/

--- front/agendamento-app/src/App.tsx
+++ front/agendamento-app/src/App.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AgendarHorario from './pages/AgendaHorarios/AgendaHorario';
-import SignUpFormProfessional from './pages/SignUpFormProfessional/SignUpFormProfessional';
-import SignUpFormUser from './pages/SignUpFormUser/SignUpFormUser';
 
 function App() {
 
