79cd9fa0d384b12d3fc551c4d3ef515021b7eb9a
WeslleyIvis <weslley_reive@hotmail.com>
2025-05-17 03:30:09 -0300

    Select Hour, Select Professional, Btn

--- front/agendamento-app/src/pages/AgendaHorarios/AgendaHorario.tsx
+++ front/agendamento-app/src/pages/AgendaHorarios/AgendaHorario.tsx
@@ -1,8 +1,13 @@
 import { useEffect, useState, useRef } from 'react';
+import { Professional } from '../../models/Professional';
 
 import MenuMobile from '../../components/menu_mobile/MenuMobile';
 import ButtonVoltar from '../../components/ButtonVoltar/ButtonVoltar';
 import SelectDay from '../../components/SelectDay/SelectDay';
+import SelectHour from '../../components/SelectHour/SelectHour';
+
+import styles from './AgendaHorario.module.scss'
+import SelectProfessional from '../../components/SelectProfessional/SelectProfessional';
 
 export type DayFormat = {
     day: number,
@@ -10,6 +15,7 @@ export type DayFormat = {
 }
 
 const AgendaHorario = () => {
+    const [selectedProfessional, setSelectedProfessional] = useState<Professional | undefined>(undefined)
     const [days, setDays] = useState<DayFormat[]>([])
     const [countDays, setCountDays] = useState<number>(0)
     const [nextWeek, setNextWeek] = useState(0)
@@ -21,25 +27,45 @@ const AgendaHorario = () => {
 
     const workDays = [ "terça-feira", "quarta-feira", "quinta-feira", "sexta-feira", "sábado" ]
 
+    const [viewDays, setViewDays] = useState<number>(7);
+
+    const updateViewDays = () => {
+        const width = window.innerWidth;
+            if (width < 360) {
+            setViewDays(5); // S Mobile
+        } else if (width < 425) {
+            setViewDays(6); // M Mobile
+        } else {
+            setViewDays(7); // L Mobile
+        }
+    }
+
+    // Evento de redimensionamento da janela
     useEffect(() => {
+        updateViewDays();
+        
+        window.addEventListener('resize', updateViewDays);
+        return () => window.removeEventListener('resize', updateViewDays);
+    }, [])
+
+    useEffect(() => {
+        const formatter = new Intl.DateTimeFormat('pt-BR', {weekday: 'long'})
         const newDays: DayFormat[] = []
         let i = countDays;
-
-        while(newDays.length < 7) {
+        
+        while(newDays.length < viewDays) {
 
             const dayloop = new Date(date)
             dayloop.setDate(date.getDate() + i)
 
-            workDays.forEach((value) => {
-                if(value == Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(dayloop)) 
-                {
-                    const today: DayFormat = {
-                        day: dayloop.getDate(),
-                        weekday: Intl.DateTimeFormat('pt-BR', {weekday: 'long'}).format(dayloop)
-                    }
-                    newDays.push(today)
-                }
-            })
+            const weekday = formatter.format(dayloop)
+
+            if(workDays.includes(weekday)) {
+                newDays.push({
+                    day: dayloop.getDate(),
+                    weekday
+                })
+            }
             i++
         }
         
@@ -53,13 +79,29 @@ const AgendaHorario = () => {
 
         setNextWeek(i)
         setDays(newDays)
-    }, [countDays]) 
+    }, [countDays, viewDays, selectedProfessional]) 
 
   return (
     <>
-      <ButtonVoltar placeholder="Agendar Horário"/>
-      <SelectDay days={days} nameMonth={UperCaseNameMonth} handlerWeekRef={handlerWeekRef} nextWeek={nextWeek} backWeek={1} setCountDays={setCountDays}/>
-      <MenuMobile />
+        <div className={styles.mainContainer}>
+            <ButtonVoltar placeholder="Agendar Horário"/>
+            <SelectDay 
+                days={days}
+                nameMonth={UperCaseNameMonth}
+                handlerWeekRef={handlerWeekRef}
+                nextWeek={nextWeek} 
+                setCountDays={setCountDays}
+            />
+            <SelectHour professional={selectedProfessional}/>
+            <SelectProfessional 
+                setSaveSeletectedProfessional={setSelectedProfessional}
+            />
+            <div className='container-button'>
+                <button type='button' className="button-primary">Continuar</button>
+            </div>
+        </div>
+       
+        <MenuMobile />
     </>
   );
 };
