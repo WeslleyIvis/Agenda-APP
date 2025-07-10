2f4c78a484cc19d4a499d520e7d9b276183e9d45
WeslleyIvis <weslley_reive@hotmail.com>
2025-07-10 15:36:22 -0300

    Login Page

--- front/agendamento-app/src/pages/LoginScreen/LoginScreen.tsx
+++ front/agendamento-app/src/pages/LoginScreen/LoginScreen.tsx
@@ -1,65 +1,76 @@
 import { useState } from 'react';
 
 import styles from './LoginScreen.module.scss';
-import { CiUser } from 'react-icons/ci';
-import { FaRegEye } from 'react-icons/fa';
+import { icons } from '../../assets/icons';
+import AutForm from '../../components/AutoForm/AutoForm';
+
+interface LoginFormValues {
+	email: string;
+	password: string;
+}
 
 const inputsForms = [
-  {
-    icon: '',
-    name: "email",
-    type:"email",
-    placeholder: "Email",
-  },
-  {
-    icon: '',
-    name: "password",
-    type:"password",
-    placeholder: "Senha",
-  }
+	{
+		icon: icons.icon_user,
+		name: "email",
+		type:"email",
+		placeholder: "Email",
+	},
+	{
+		icon: icons.icon_eye,
+		name: "password",
+		type:"password",
+		placeholder: "Senha",
+	}
 ]
 
 const LoginScreen = () => {
-    const [form, setForm] = useState({email: '', password: ''})
+		const [loginFormValues, setForm] = useState<LoginFormValues>({
+			email: '',
+			password: ''
+		})
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(e)
-        setForm({ ...form, [e.target.name]: e.target.value})
-    }
+		const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+				console.log(e)
+				setForm({ ...loginFormValues, [e.target.name]: e.target.value})
+		}
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        console.log("From data:", form)
-    }
+		const handleSubmit = (e: React.FormEvent) => {
+				e.preventDefault();
+				console.log("From data:", loginFormValues)
+		}
 
-  return (
-    <div>
-        <div className={styles.bgImage}></div>
-        <section>
-            <h1>HGBG</h1>
-            <div>
-                <h2>Bem Vindo</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing eli,</p>
-            </div>
-            <form onSubmit={handleSubmit}>
-                {inputsForms.map((item, i) => (
-                    <label key={i}>
-                        <input 
-                            type={item.type} 
-                            name={item.name}
-                            value={form[item.name as keyof typeof form]}
-                            placeholder={item.placeholder}
-                            onChange={handleChange}
-                            required
-                        />
-                    </label> 
-                ))}
-                <button type='submit'>Entrar</button>
-            </form>
+	return (
+		<div>
+			<div className={styles.bgImage}></div>
+			<section className='container-login-screen'>
+				<h1 className='form-title'>HGBG</h1>
+				<div className={styles.infoLogin}>
+					<h2 className='form-subtitle'>Bem Vindo</h2>
+					<p className='form-text'>Lorem ipsum dolor sit amet, consectetur adipiscing eli,</p>
+				</div>
+				<AutForm 
+					items={inputsForms} 
+					formValues={loginFormValues as any}
+					handleChange={handleChange}
+					handleSubmit={handleSubmit}
+				/>
+					
+				<div style={{paddingBottom: '50px'}}>
+ 					<a href="">Esqueceu sua senha? </a>
+				</div>
+				<div className={styles.containerRegister}>
+						<p>Não tem conta? 
+							<b>
+								<a href="/create"> Cadastre-se</a>
+							</b>
+						</p>
+				</div>
 
-        </section>
-    </div>
-  );
+				
+			</section>
+		</div>
+	);
 };
 
 export default LoginScreen;
