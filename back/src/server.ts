be05913ca9e1a352651db9699c87f81c3a4b6bd1
WeslleyIvis <weslley_reive@hotmail.com>
2025-07-24 22:33:46 -0300

    localhost 3001

--- back/src/server.ts
+++ back/src/server.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGO_URI as string).then(() => {
     console.log('Conectado ao MongaoDB!')
-    app.listen(3001, '192.168.1.121',  () => {
+    app.listen(3001,  () => {
         console.log(`Servido rodando: http://localhost:3001`)
         })
     })
